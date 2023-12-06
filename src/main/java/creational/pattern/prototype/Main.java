9c59dc3341e037c5a7ea6e182b27ebdbc07fed42
aganesan <Anish.Ganesan@viasat.com>
2023-12-07 00:31:58 +0530

    add helper to prototype class

--- src/main/java/creational/pattern/prototype/Main.java
+++ src/main/java/creational/pattern/prototype/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Vehicle bus1 = new Bus(4, 989899, "green");
         Vehicle clone1 = bus1.clone();
-        clone1.isClone(bus1);
+        clone1.isClone(bus1); // Proper clone
 
         Vehicle cycle1 = new Cycle(4, 989899, true);
         Vehicle cycle2 = cycle1.clone();
 
-        cycle1.isClone(cycle2);
+        cycle1.isClone(cycle2);  // Proper clone
 
-        cycle1.isClone(bus1);
+        cycle1.isClone(bus1);  // These are NOT the right clones (bus != cycle objects)
     }
 }
